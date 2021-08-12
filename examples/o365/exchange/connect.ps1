249afa198de77106ef028206127638b3e06520b7
Tripp Thrift <tripp@trippt.xyz>
2021-08-11 22:45:53 -0400

    O365 addition

---
+++ examples/o365/exchange/connect.ps1
@@ -0,0 +1,6 @@
+# Import pre-requisite
+Import-Module ExchangeOnlineManagement
+# Connect
+Connect-ExchangeOnline
+
+# PSGallery Module Overview https://www.powershellgallery.com/packages/ExchangeOnlineManagement/2.0.5
